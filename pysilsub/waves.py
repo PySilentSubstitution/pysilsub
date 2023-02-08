3fa42e0252a665a3950a0ed21917b1c8aeb6cba5
Joel Martin <11196943+jtmbeta@users.noreply.github.com>
2023-02-08 11:32:33 +0000

    Add waves.py /JTM

---
+++ pysilsub/waves.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  5 18:28:20 2023
+
+@author: jtm545
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def make_stimulus_waveform(
+    frequency: int | float,
+    sampling_frequency: int,
+    phase: int | float = 0,
+    duration: int | float = 1,
+):
+    """Make sinusoidal waveform.
+
+    Can be used as a contrast template for silent substitution contrast
+    modulations.
+
+
+    Parameters
+    ----------
+    frequency : int or float
+        Temporal frequency of the sinusoid.
+    sampling_frequency : int
+        Sampling frequency of the modulation. This should not exceed the
+        temporal resolution (spectral switching time) of the stimulation
+        device.
+    phase : float, optional
+        Phase offset in radians. The default is `0`, which gives a sine wave.
+    duration : int or flaot, optional
+        Duration of the modulation in seconds. The default is 1, becasuse it
+        is easy to repeat a sinusoidal modulation multiple times and add a new
+        timebase.
+
+    Returns
+    -------
+    pd.Series
+        Sinusoidal waveform with time index.
+
+    """
+    sampling_interval = 1.0 / sampling_frequency
+    time = np.arange(0, duration, sampling_interval)
+    waveform = np.sin(2 * np.pi * frequency * time + phase)
+    return pd.Series(waveform, index=time)
